3c161cd8a27d335473c02bcad14586c5d1af700a
hsrussel <hsrussel@ncsu.edu>
2017-09-30 21:31:41 -0400

    added screen swtich to a button

--- MyApplication/app/src/main/java/com/example/beth/myapplication/KitDisplayPack.java
+++ MyApplication/app/src/main/java/com/example/beth/myapplication/KitDisplayPack.java
@@ -1,13 +1,23 @@
 package com.example.beth.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class KitDisplayPack extends AppCompatActivity {
+    public static final String PACK_TAB = "com.example.myfirstapp.PACK_TAB";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_kit_display_pack);
+    }
+
+    /** Called when the user taps the Send button */
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, KitDisplay.class);
+        startActivity(intent);
     }
 }
