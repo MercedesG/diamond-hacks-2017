1f03a4e338b887ac54eccf62519819e858bb5a20
ecmikovi <ecmikovi@ncsu.edu>
2017-09-30 21:52:44 -0400

    added new layouts

---
+++ MyApplication/app/src/main/java/com/example/beth/myapplication/DisplayLists.java
@@ -0,0 +1,13 @@
+package com.example.beth.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DisplayLists extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_lists);
+    }
+}
